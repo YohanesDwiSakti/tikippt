75a574c9d3c79b039366bb7442d44c8418471c44
YohanesDwiSakti <YohanesDwiSakti@users.noreply.github.com>
2026-06-07 17:20:01 +0700

    Add Vercel Laravel deployment

---
+++ apps/web/api/index.php
@@ -0,0 +1,20 @@
+<?php
+
+foreach ([
+    '/tmp/finproppt/cache',
+    '/tmp/finproppt/sessions',
+    '/tmp/finproppt/views',
+] as $directory) {
+    if (! is_dir($directory)) {
+        mkdir($directory, 0755, true);
+    }
+}
+
+putenv('CACHE_STORE=array');
+putenv('SESSION_DRIVER=cookie');
+putenv('VIEW_COMPILED_PATH=/tmp/finproppt/views');
+$_ENV['CACHE_STORE'] = 'array';
+$_ENV['SESSION_DRIVER'] = 'cookie';
+$_ENV['VIEW_COMPILED_PATH'] = '/tmp/finproppt/views';
+
+require __DIR__ . '/../public/index.php';
